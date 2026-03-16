8c55a78e24130bbad1b1b9c29cad0c3aee83eda8
RihabMhd <rihabmahdi19@gmail.com>
2026-03-16 11:41:30 +0000

    Add initial project structure with API, web, and console routes
    - Created api.php for API routes including authentication and user profile management.
    - Added channels.php for event broadcasting channel authorization.
    - Established console.php for defining Closure based console commands.
    - Set up web.php for web routes with a welcome view.
    - Configured .gitignore files for various storage directories to exclude unnecessary files from version control.
    - Implemented basic test structure with ExampleTest for feature and unit testing.
    - Introduced Vite configuration for asset management in the project.

---
+++ AlMadar_Bank/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterUser;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AuthController extends Controller
+{
+    public function register(RegisterUser $request): JsonResponse
+    {
+        $user = User::create(
+            array_merge(
+                $request->validated(),
+                ['password' => Hash::make($request->password)]
+            )
+        );
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(['message' => 'Account created successfully'], 201);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token
+        ], 200);
+    }
+
+    public function logout(): JsonResponse
+    {
+        auth('api')->logout();
+        return response()->json(['message' => 'Logout successful'], 200);
+    }
+
+    public function refresh(): JsonResponse
+    {
+        $token = Auth::refresh();
+        return response()->json(['token' => $token]);
+    }
+}
