dbd4b2b42e541a5cfbb8b8662c0f38d833e43a1f
RihabMhd <rihabmahdi19@gmail.com>
2026-03-16 12:23:53 +0000

    files of my project

--- AlMadar_Bank/routes/api.php
+++ AlMadar_Bank/routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Api\ProfileController;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the "api" middleware group. Make something great!!!!
 |
 */
 
