05cb04ee1cb38daec8473aac06bf95c706760a10
RihabMhd <rihabmahdi19@gmail.com>
2026-03-16 12:52:17 +0000

    Refactor API routes: streamline authentication and user profile routes

--- AlMadar_Bank/routes/api.php
+++ AlMadar_Bank/routes/api.php
@@ -1,30 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Api\ProfileController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!!!!
-|
-*/
-
-
-
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login',    [AuthController::class, 'login']);
+});
 
-    Route::middleware('auth:api')->group(function () {
-        Route::post('logout',  [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-        Route::get('me',       [AuthController::class, 'me']);
-    });
+
+Route::middleware('auth:api')->group(function () {
+
+
+    Route::post('auth/refresh', [AuthController::class, 'refresh']);
+    Route::post('auth/logout',  [AuthController::class, 'logout']);
+
+    Route::get('users/me',              [ProfileController::class, 'me']);
+    Route::put('users/me',              [ProfileController::class, 'updateProfile']);
+    Route::patch('users/me/password',   [ProfileController::class, 'updatePassword']);
 });
